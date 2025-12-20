4c8f34b688feaa0b8e4a74aa5b096aab291cb637
Cassio <cassiofelipepaiva@gmal.com>
2025-12-20 15:06:24 -0300

    primeiro commit

---
+++ academiadev/src/main/java/com/example/academiadev/AcademiadevApplication.java
@@ -0,0 +1,13 @@
+package com.example.academiadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiadevApplication.class, args);
+	}
+
+}
---
+++ academiadev/src/test/java/com/example/academiadev/AcademiadevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academiadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiadevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
